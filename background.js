2dd97fa6c94b71410e09854b2e20e0887385cdab
Juan Diego Urena <juandiego89@hotmail.com>
2023-11-07 20:08:38 +0900

    replace setTimeout with chrome alarms for auto mining

--- background.js
+++ background.js
@@ -1,13 +1,33 @@
-let timerId = null;
 let remainingTime = null;
+const getAlarm = () => {
+    chrome.alarms.get('mine-celia-alarm').then((alarm) => {
+        if (alarm) {
+            // Do something when the user clicks the extension
+            remainingTime = new Date(alarm.scheduledTime).getTime() - new Date().getTime();
+            chrome.runtime.sendMessage(remainingTime);
+        }
+
+    });
+}
+
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    if (message.action === 'userClick' && timerId !== null) {
+    if (message.action === 'userClick') {
         // Do something when the user clicks the extension
-        const msToEnd = remainingTime - new Date().getTime();
-        console.log('User clicked the extension.', timerId, msToEnd);
-        chrome.runtime.sendMessage(msToEnd);
+        getAlarm();
     }
 });
+
+chrome.alarms.onAlarm.addListener((alarm) => {
+    chrome.tabs.query({}, (tabs) => {
+        const celiaTab = tabs.find((tab) => tab.url.startsWith("https://app.celia.finance/app/mine"));
+        if (celiaTab) {
+            chrome.tabs.reload(celiaTab.id)
+        } else {
+            chrome.tabs.create({ url: "https://app.celia.finance/app/mine", active: false });
+        }
+    });
+});
+
 const miner = async () => {
     return new Promise((resolve) => {
         const waitForAnyElement = (selectors) => {
@@ -33,45 +53,35 @@ const miner = async () => {
             const hours = Array.from(document.querySelectorAll("h4")).find(el => el.textContent === "Hour(s)")?.previousElementSibling?.textContent;
             const minutes = Array.from(document.querySelectorAll("h4")).find(el => el.textContent === "Minute(s)")?.previousElementSibling?.textContent;
             const seconds = Array.from(document.querySelectorAll("h4")).find(el => el.textContent === "Second(s)")?.previousElementSibling?.textContent;
-            console.log("finding the timer ", hours, minutes, seconds);
             if (hours && minutes && seconds) {
-                totalMilliseconds = (parseInt(hours) * 3600000 + parseInt(minutes) * 60000 + parseInt(seconds) * 1000) + 300000;
+                totalMilliseconds = parseInt(hours) * 60 + parseInt(minutes) + parseInt(seconds) / 60;
             } else {
                 const mineBtn = document.getElementsByClassName("modal-groups")[0];
                 console.log("mineBtn", mineBtn)
                 if (!mineBtn) window.location.reload();
                 mineBtn.firstElementChild.click()
-                totalMilliseconds = 28800000 + 300000;
+                totalMilliseconds = 485;
                 setTimeout(() => {
                     document.getElementsByClassName("btn-special")[1].click()
                 }, 4000)
             }
-            resolve(totalMilliseconds);
+            resolve(totalMilliseconds + 3);
         })
     });
 }
+
+chrome.alarms.clearAll();
+
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status === 'complete' && String(tab.url).includes('celia.finance')) {
-        console.log(tab.url);
         chrome.scripting.executeScript({
             target: { tabId: tab.id },
             function: miner
-        }, (result) => {
-            if (result[0]?.result > 0) {
+        }, async (result) => {
+            if (result && result[0]?.result > 0) {
                 const delay = result[0].result;
-                clearTimeout(timerId);
-                remainingTime = new Date().getTime() + delay;
-                timerId = setTimeout(() => {
-                    chrome.tabs.query({}, (tabs) => {
-                        const celiaTab = tabs.find((tab) => tab.url.startsWith("https://app.celia.finance/app/mine"));
-                        if (celiaTab) {
-                          chrome.tabs.reload(celiaTab.id)
-                        } else {
-                          chrome.tabs.create({ url: "https://app.celia.finance/app/mine", active: false });
-                        }
-                      });
-                }, delay);
-                chrome.runtime.sendMessage(result[0].result);
+                await chrome.alarms.create('mine-celia-alarm', { delayInMinutes: delay })
+                getAlarm();
             }
         })
     }
