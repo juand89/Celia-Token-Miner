c2376fc83b5984f202a0bab52ffeff4cdc64ebcf
Juan Diego Urena <juandiego89@hotmail.com>
2023-11-18 13:32:10 +0900

    refactor logic to include mutation observer

--- background.js
+++ background.js
@@ -1,47 +1,65 @@
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    console.log(message);
-  });
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  console.log(message);
+});
 const miner = async () => {
-    await new Promise((resolve) =>
-    {
-        const setTimeoutHandler = ()=> {
-            // chrome.runtime.sendMessage("send message from miner");
-            const hours = Array.from(document.querySelectorAll("h4")).find(el => el.textContent === "Hour(s)")?.previousElementSibling?.textContent;
-            const minutes = Array.from(document.querySelectorAll("h4")).find(el => el.textContent === "Minute(s)")?.previousElementSibling?.textContent;
-            const seconds = Array.from(document.querySelectorAll("h4")).find(el => el.textContent === "Second(s)")?.previousElementSibling?.textContent;
-            if (hours && minutes && seconds) {
-                const totalMilliseconds = (parseInt(hours) * 3600000 + parseInt(minutes) * 60000 + parseInt(seconds) * 1000) + 180000;
-                setTimeout(() => {
-                    document.location.reload();
-                }, totalMilliseconds)
-            } else {
-              const mineBtn = document.getElementsByClassName("modal-groups")[0];
-              console.log("mineBtn", mineBtn)
-              if (!mineBtn) window.location.reload();
-              mineBtn.firstElementChild.click()
-              setTimeout(() => {
-                  document.getElementsByClassName("btn-special")[1].click()
-                  setTimeout(() => {
-                    window.location.reload()
-                }, 4000)
-              }, 4000)
+  await new Promise((resolve) => {
+    const waitForAnyElement = (selectors) => {
+      return new Promise((resolve) => {
+        const observer = new MutationObserver((mutationsList) => {
+          mutationsList.forEach((mutation) => {
+            for (const selector of selectors) {
+              const elements = mutation.target.querySelectorAll(selector);
+              if (elements.length > 0) {
+                observer.disconnect();
+                resolve(elements[0]);
+              }
             }
-            resolve();
-        }
-      setTimeout(setTimeoutHandler, 3000);
-    })
-    miner();
-}
-let scriptExecuted = false;
-  chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-      if (changeInfo.status === 'complete' && String(tab.url).includes('celia.finance')) {
-        if (!scriptExecuted) {
-            chrome.scripting.executeScript({
-              target: { tabId: tab.id },
-              function: miner,
-            }, () => {
-                scriptExecuted = false;
-            })
+          });
+        });
+        observer.observe(document.body, { childList: true, subtree: true });
+      });
+    };
+    waitForAnyElement(["h4", ".modal-groups"]).then((element) => {
+      console.log(element, "element")
+      if (element.constructor.name === "HTMLHeadingElement") {
+        const hours = Array.from(document.querySelectorAll("h4")).find(el => el.textContent === "Hour(s)")?.previousElementSibling?.textContent;
+        const minutes = Array.from(document.querySelectorAll("h4")).find(el => el.textContent === "Minute(s)")?.previousElementSibling?.textContent;
+        const seconds = Array.from(document.querySelectorAll("h4")).find(el => el.textContent === "Second(s)")?.previousElementSibling?.textContent;
+        const totalMilliseconds = (parseInt(hours) * 3600000 + parseInt(minutes) * 60000 + parseInt(seconds) * 1000) + 180000;
+        console.log({ totalMilliseconds })
+        setTimeout(() => {
+          resolve();
+          document.location.reload();
+        }, totalMilliseconds)
+      } else {
+        const mineBtn = document.querySelector(".modal-groups");
+        if (mineBtn) {
+          console.log("mineBtn", mineBtn)
+          mineBtn.firstElementChild.click()
+          waitForAnyElement(["btn-special"]).then(() => {
+            document.getElementsByClassName("btn-special")[0].click()
+            setTimeout(() => {
+              resolve();
+              document.location.reload()
+            }, 4000)
+          })
         }
       }
+
+    })
   })
+}
+let scriptExecuted = false;
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'complete' && String(tab.url).includes('celia.finance')) {
+    if (!scriptExecuted) {
+      scriptExecuted = true;
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: miner,
+      }, () => {
+        scriptExecuted = false;
+      })
+    }
+  }
+})
